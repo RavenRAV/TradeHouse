23ecc4a623195d0e8019e9c9f29a61e332d84ca3
Khan-Aiym <hanaiym9@gmail.com>
2023-02-14 21:15:18 +0600

    UI_khan_aiym

---
+++ app/src/main/java/com/geektech/tradehouse/presentation/ui/fragments/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.khan_ayym
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.geektech.tradehouse.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
